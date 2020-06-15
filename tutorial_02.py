b48ae8d0e826e74f770ccd361f55dc595124379b
Jiyeon Lee <jy_luda@thetrive.com>
2020-06-15 17:14:07 +0900

    Update

--- tutorial_02.py
+++ tutorial_02.py
@@ -17,45 +17,24 @@ names = [
 ]
 df = pd.read_csv('carmanager.csv', sep=',', names=names)
 
-def to_hour(row):
-    ts = row.get('timestamp', 0)
-    return dt.datetime.utcfromtimestamp(ts).hour
-
-def to_year(row):
-    ts = row.get('timestamp', 0)
-    return dt.datetime.utcfromtimestamp(ts).year
-
-def to_month(row):
-    ts = row.get('timestamp', 0)
-    return dt.datetime.utcfromtimestamp(ts).month
-
-def to_day(row):
-    ts = row.get('timestamp', 0)
-    return dt.datetime.utcfromtimestamp(ts).day
-
 def gen_nm_yr(row) -> str:
-    nm = row.get('name', 'xxx')
-    yr = row.get('year', 'xxx')
-    return f'{nm} {yr}'
+    nm = row.get('name', 'xxx').strip()
+    yr = str(row.get('year', 'xxx')).strip()
+    return f'{nm or "xxx"} {yr or "xxx"}'
 
 def adjust_price(row) -> int:
     return int(row.get('price', 0) / 10_000)
 
 df['name_year'] = df.apply(lambda x: gen_nm_yr(x), axis=1)
 df['price'] = df.apply(lambda x: adjust_price(x), axis=1)
-df['xhour'] = df.apply(lambda x: to_hour(x), axis=1)
-df['xyear'] = df.apply(lambda x: to_year(x), axis=1)
-df['xmonth'] = df.apply(lambda x: to_month(x), axis=1)
-df['xday'] = df.apply(lambda x: to_day(x), axis=1)
 
 df = df[~df['name_year'].str.contains('xxx')]
 df = df[df['price'] != 0]
-df = df.drop_duplicates(subset=['price', 'name_year', 'plate_num', 'xyear', 'xmonth', 'xday'])
+df = df.drop_duplicates(subset=['price', 'name_year', 'plate_num'])
 df = df.drop([
     'transmission',
     'fuel',
     'options',
-    'timestamp',
     'name',
     'year',
     'regist_year',
@@ -69,7 +48,7 @@ df = df.drop([
 # mileage
 for ny in df.name_year.unique():
     dfx = df[df.name_year == ny]
-    dfx['No'] = np.arange(len(dfx))
-    dfx = dfx[['No', 'xyear', 'xmonth', 'xday', 'xhour', 'price', 'mileage']]
-    dfx.to_csv(os.path.join('data', f'{ny}.csv'), index=False)
+    dfx = dfx[['timestamp', 'price', 'mileage']]
+    if len(dfx.index) >= 50:
+        dfx.to_csv(os.path.join('data', f'{ny}.csv'), index=False)
 
--- tutorial_03.py
+++ tutorial_03.py
@@ -1,6 +1,10 @@
+from PIL import Image
+import os
+from datetime import datetime
 from math import sqrt
 from numpy import concatenate
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+import matplotlib.font_manager as fm
 from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
@@ -10,91 +14,105 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+font_name = 'NotoSansKR-Light.otf'
+font_size = 12
+fontprop = fm.FontProperties(fname=font_name, size=font_size)
 
 # convert series to supervised learning
+# A time series is a sequence of numbers that are ordered by a time index.
+# supervised learning problem is comprised of
+# input patterns (X) and output patterns (y),
+# such that an algorithm can learn how to
+# predict the output patterns from the input
+# patterns.
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-	n_vars = 1 if type(data) is list else data.shape[1]
-	df = DataFrame(data)
-	cols, names = list(), list()
+        n_vars = 1 if type(data) is list else data.shape[1]
+        df = DataFrame(data)
+        cols, names = list(), list()
 	# input sequence (t-n, ... t-1)
-	for i in range(n_in, 0, -1):
-		cols.append(df.shift(i))
-		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-	# forecast sequence (t, t+1, ... t+n)
-	for i in range(0, n_out):
-		cols.append(df.shift(-i))
-		if i == 0:
-			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-		else:
-			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+        for i in range(n_in, 0, -1):
+            cols.append(df.shift(i))
+            names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+
+        for i in range(0, n_out):
+	    # forecast sequence (t, t+1, ... t+n)
+            cols.append(df.shift(-i))
+            if i == 0:
+                names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+            else:
+                names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
 	# put it all together
-	agg = concat(cols, axis=1)
-	agg.columns = names
+        agg = concat(cols, axis=1)
+        agg.columns = names
 	# drop rows with NaN values
-	if dropnan:
-		agg.dropna(inplace=True)
-	return agg
+        if dropnan:
+            agg.dropna(inplace=True)
+        return agg
 
-# load dataset
-dataset = read_csv('pollution.csv', header=0, index_col=0)
-dataset = dataset.drop(['cbwd'], axis=1)
-print(dataset.head())
-values = dataset.values
-# integer encode direction
-encoder = LabelEncoder()
-values[:,4] = encoder.fit_transform(values[:,4])
-# ensure all data is float
-values = values.astype('float32')
-# normalize features
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaled = scaler.fit_transform(values)
-# specify the number of lag hours
-n_hours = 3
-n_features = 7
-# frame as supervised learning
-reframed = series_to_supervised(scaled, n_hours, 1)
-print(reframed.shape)
-
-# split into train and test sets
-values = reframed.values
-n_train_hours = 365 * 24
-train = values[:n_train_hours, :]
-test = values[n_train_hours:, :]
-# split into input and outputs
-n_obs = n_hours * n_features
-train_X, train_y = train[:, :n_obs], train[:, -n_features]
-test_X, test_y = test[:, :n_obs], test[:, -n_features]
-print(train_X.shape, len(train_X), train_y.shape)
-# reshape input to be 3D [samples, timesteps, features]
-train_X = train_X.reshape((train_X.shape[0], n_hours, n_features))
-test_X = test_X.reshape((test_X.shape[0], n_hours, n_features))
-print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
-
-# design network
-model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(1))
-model.compile(loss='mae', optimizer='adam')
-# fit network
-history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-# plot history
-pyplot.plot(history.history['loss'], label='train')
-pyplot.plot(history.history['val_loss'], label='test')
-pyplot.legend()
-pyplot.show()
-
-# make a prediction
-yhat = model.predict(test_X)
-test_X = test_X.reshape((test_X.shape[0], n_hours*n_features))
-# invert scaling for forecast
-inv_yhat = concatenate((yhat, test_X[:, -7:]), axis=1)
-inv_yhat = scaler.inverse_transform(inv_yhat)
-inv_yhat = inv_yhat[:,0]
-# invert scaling for actual
-test_y = test_y.reshape((len(test_y), 1))
-inv_y = concatenate((test_y, test_X[:, -7:]), axis=1)
-inv_y = scaler.inverse_transform(inv_y)
-inv_y = inv_y[:,0]
-# calculate RMSE
-rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-print('Test RMSE: %.3f' % rmse)
+for fname in [x for x in os.listdir('data') if '.csv' in x]:
+    # load dataset
+    dataset = read_csv(os.path.join('data', fname), header=0, index_col=0)
+    num_rows = len(dataset.index)
+    #dataset.iloc[:, 0] = datetime.fromtimestamp(dataset.iloc[:, 0])
+    #dataset = dataset[['date', 'price', 'mileage']]
+    values = dataset.values
+    # ensure all data is float
+    values = values.astype('float32')
+    # normalize features
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
+    # specify the number of lag hours
+    n_hours = 5
+    n_features = 2
+    # frame as supervised learning
+    reframed = series_to_supervised(scaled, n_hours, 1)
+    
+    # split into train and test sets
+    values = reframed.values
+    n_train_hours = num_rows // 2
+    train = values[:n_train_hours, :]
+    test = values[n_train_hours:, :]
+    # split into input and outputs
+    n_obs = n_hours * n_features
+    train_X, train_y = train[:, :n_obs], train[:, -n_features]
+    test_X, test_y = test[:, :n_obs], test[:, -n_features]
+    # reshape input to be 3D [samples, timesteps, features]
+    train_X = train_X.reshape((train_X.shape[0], n_hours, n_features))
+    test_X = test_X.reshape((test_X.shape[0], n_hours, n_features))
+    
+    # design network
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+    model.add(Dense(1))
+    model.compile(loss='mae', optimizer='adam')
+    # fit network
+    history = model.fit(train_X, train_y, epochs=50, batch_size=2, validation_data=(test_X, test_y), verbose=0, shuffle=True)
+    
+    # make a prediction
+    yhat = model.predict(test_X)
+    test_X = test_X.reshape((test_X.shape[0], n_hours*n_features))
+    # invert scaling for forecast
+    inv_yhat = concatenate((yhat, test_X[:, -1:]), axis=1)
+    inv_yhat = scaler.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[:,0]
+    # invert scaling for actual
+    test_y = test_y.reshape((len(test_y), 1))
+    inv_y = concatenate((test_y, test_X[:, -1:]), axis=1)
+    inv_y = scaler.inverse_transform(inv_y)
+    inv_y = inv_y[:,0]
+    # calculate RMSE
+    rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+    rmse = ('RMSE: %.3f' % rmse)
+    
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='test')
+    car_name = fname.replace('.csv', '')
+    title = f'{car_name}: {rmse}'
+    print(title)
+    plt.title(title, fontproperties=fontprop)
+    plt.legend()
+    #plt.show()
+    im_name = os.path.join('data_images', f'{car_name}.png')
+    plt.savefig(im_name)
+    Image.open(im_name).save(im_name, 'PNG')
+    plt.clf()
