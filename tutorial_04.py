b29f1be5c392726658748772bff4a88821646ef1
Jiyeon Lee <jy_luda@thetrive.com>
2020-06-19 09:46:59 +0900

    Update

--- tutorial_04.py
+++ tutorial_04.py
@@ -32,25 +32,35 @@ def gen_nm_yr(row) -> str:
     return norm(f'{nm or "xxx"} {yr or "xxx"}')
 
 def adjust_price(row) -> int:
-    return int(row.get('price', 0) / 10_000)
+    return int(row.get('price', 0))
+
+def format_price(row) -> str:
+    num = row.get('price')
+    return f"{num:,}"
+
+def format_mileage(row) -> str:
+    num = row.get('mileage')
+    return f"{num:,}KM"
+
+def format_regist_year(row) -> str:
+    num = row.get('regist_year', '0000.00.00')
+    return str(num).replace('-', '.').strip()
+
 
 df['name_year'] = df.apply(lambda x: gen_nm_yr(x), axis=1)
+df['regist_year'] = df.apply(lambda x: format_regist_year(x), axis=1)
 df['price'] = df.apply(lambda x: adjust_price(x), axis=1)
 df['plate_num'] = df.apply(lambda x: norm(x.get('plate_num')), axis=1)
-df['date'] = df.apply(lambda x: dt.utcfromtimestamp(x.get('timestamp')).strftime('%Y-%m-%d'), axis=1)
+df['date'] = df.apply(lambda x: dt.utcfromtimestamp(x.get('timestamp')).strftime('%Y.%m.%d'), axis=1)
 
 df = df[~df['name_year'].str.contains('xxx')]
 df = df[df['price'] != 0]
-df = df.drop_duplicates(subset=['price', 'name_year', 'plate_num'])
-df = df.drop([
-    'transmission',
-    'options',
-    'name',
-    'year',
-    'regist_year',
-    'timestamp',
-], axis=1)
-df = df[['date', 'name_year', 'plate_num', 'price', 'mileage', 'color', 'no_accident', 'yes_warranty', 'fuel']]
+df['price'] = df.apply(lambda x: format_price(x), axis=1)
+df['mileage'] = df.apply(lambda x: format_mileage(x), axis=1)
+#df = df.drop_duplicates(subset=['price', 'name_year', 'plate_num'])
+df = df[['date', 'name', 'price', 'year', 'regist_year', 'mileage', 'transmission', 'fuel', 'plate_num', 'color', 'no_accident', 'yes_warranty']]
 df = df.sort_values(by=['date'])
+headers = ['날짜', '모델명', '차량가격', '형식년도', '최초등록일', '마일리지', '변속', '연료', '자동차 번호', '색상', '무사고', '워런티']
+df.columns = [norm(x) for x in headers]
 df.to_csv(os.path.join('phoenix', f'phoenix.csv'), index=False)
 df.to_excel(os.path.join('phoenix', 'phoenix.xlsx'))
